daad3d1ec6dc458b3dbad87d1b3cffe200799511
alllangsg <aserrato@unal.edu.co>
2024-02-20 20:11:35 +0100

    Spotify Token 8:11pm

--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,16 @@
-const apiToken: string = '';
+const apiToken: string = 'BQAhiEZqAuC77BHS8IzeIof-JdDGhBKZZHWDwPEnLrEKqZyDTQ-1tz_Ni3Cm_B-AKbcURxK-Tx-xpN7WIaEMLZ_goRTIyvhRqcYnYt0JjyBe7jmLe56cNYwMTMzm_uzQdKO6m_zdej32bQvTsr8Jzu_mMA8mIQhmcTKN-YnkS8Nf98DcnXwI_ObWzfuxW-nlUC9REijJ3GF_zXSv5HE7KVZ9j2JgnA';
 
 export const fetchTracks = async () => {
-  // This function does nothing, yet
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+     throw new Error(`Fetching tracks failed with status ${response.status}`)
+   }
+  const data = await response.json() as { items: unknown[] };
+
+  return data.items;
 };
