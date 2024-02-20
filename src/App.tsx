19b0e1ecf7af7afadc9ebbf6babfb792c4e1602e
alllangsg <aserrato@unal.edu.co>
2024-02-20 19:52:29 +0100

    NextButton fixed, now it works properly

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import './App.css';
 import { useState } from 'react';
 
 const App = () => {
+
+  const [trackIndex, setTrackIndex] = useState(0)
+
+  const goToNextTrack = () => {
+    setTrackIndex(trackIndex + 1);
+  }
+
   return (
     <div className="App">
 
@@ -36,11 +43,7 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
 
-const [trackIndex, setTrackIndex] = useState(0)
 
-const goToNextTrack = () => {
-  setTrackIndex(trackIndex + 1);
-}
 
 
 export default App;
